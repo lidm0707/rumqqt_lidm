21d735851f58d306082e5e4f16ec58baf39752e9
macmoo <macmoo@macmoos-MacBook-Pro.local>
2026-06-13 02:02:22 +0700

    fix: default broker log level to info

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,12 @@ fn build_config() -> Config {
 }
 
 fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
 
     let config = build_config();
     let mut broker = Broker::new(config);
