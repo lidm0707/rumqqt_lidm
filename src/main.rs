71cecd05fef7e1fbd8da14e9c5dd2644f857bb5e
macmoo <macmoo@macmoos-MacBook-Pro.local>
2026-06-13 23:50:02 +0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("debug")),
         )
         .init();
 
